fd60c623a28a1cc46bc5a30447664f69516b93d9
Gustaf Ahdritz <gahdritz@gmail.com>
2025-05-21 19:13:55 -0400

    New repo structure

---
+++ smel/scripts/evaluate_model_on_questions.py
@@ -0,0 +1,368 @@
+import argparse
+from itertools import combinations
+import json
+import os
+import pickle
+import random
+
+import anthropic
+import datasets
+from google import genai as google_genai
+from google.api_core import exceptions
+import openai
+import torch
+import transformers
+from transformers import (
+    AutoTokenizer, 
+    AutoModelForCausalLM,
+    BitsAndBytesConfig,
+)
+from urllib.parse import urlparse
+
+from smel.utils.constants import (
+    DOMAINS,
+    URL_TO_NAME,
+    filter_combinations,
+)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--combo_id", type=int, default=0)
+parser.add_argument("--entity", type=str, default="agency")
+parser.add_argument("--model", type=str, default="llama")
+parser.add_argument("--stop_after_k", type=int, default=None)
+parser.add_argument("--use_local", action="store_true", default=False)
+args = parser.parse_args()
+
+ENTITY = args.entity
+MODEL = args.model
+
+WRITE_TO_DISK = False
+C4_JSONL = None
+C4_JSONL = "scratch/c4-0000.json"
+NO_C4_DOCUMENTS = 14
+#NO_C4_DOCUMENTS = 0
+OPENAI_BATCH = False
+OPENAI_BATCH_DIR = f"openai_batches/{MODEL}"
+RUN_NAME = f"{ENTITY}_ignoring"
+PICKLE_DIR = f"pickles"
+OUTPUT_DIR = f"{PICKLE_DIR}/{MODEL}_{RUN_NAME}/"
+PROMPT_DIR = f"{PICKLE_DIR}/{MODEL}_{RUN_NAME}_prompts/"
+
+torch.manual_seed(args.combo_id + 42)
+random.seed(args.combo_id + 43)
+
+def get_context_keys(no_docs, combo_id):
+    domain_combinations = list(combinations([url for _, url in DOMAINS], no_docs))
+     
+    if(no_docs == 2):
+        domain_combinations = [t for t in domain_combinations if filter_combinations(t)]
+    
+    print(list(enumerate(domain_combinations)))
+    context_keys = list(domain_combinations[combo_id])
+
+    return context_keys
+
+
+if(args.use_local):
+    QUESTION_FILE = f"{ENTITY}_questions_filtered.pickle"
+    CONTEXT_FILES = [
+        f"{ENTITY}_questions_filtered_rewritten.pickle",
+        f"{ENTITY}_questions_filtered_{ENTITY}_questions_filtered_rewritten_corrupted.pickle",
+    ]
+    no_docs = len(CONTEXT_FILES)
+
+    with open(os.path.join(PICKLE_DIR, QUESTION_FILE), "rb") as fp:
+        questions = pickle.load(fp)
+   
+    context_keys = get_context_keys(no_docs, args.combo_id)
+
+    context_files = []
+    for context_file, context_key in zip(CONTEXT_FILES, context_keys):
+        with open(os.path.join(PICKLE_DIR, context_file), "rb") as fp:
+            d = pickle.load(fp)
+            print(d.keys())
+            print(context_file)
+            contexts = d[context_key]
+            
+            assert(len(contexts) == len(questions))
+            context_files.append(contexts)
+
+    question_file = os.path.basename(QUESTION_FILE).rsplit('.', 1)[0]
+else:
+    datasets = [
+        datasets.load_dataset("gahdritz/smel", name="qa_rewritten", split="test"),
+        datasets.load_dataset("gahdritz/smel", name="qa_corrupted", split="test"),
+    ]
+    no_docs = len(datasets)
+    
+    context_keys = get_context_keys(no_docs, args.combo_id)
+ 
+    context_files = []
+    for d, context_key in zip(datasets, context_keys):
+        questions = [(None, '\n'.join([e["question"], e["answer"]])) for e in datasets[0] if e["source"] == context_key and e["entity"] == ENTITY]
+        contexts = [(e["passage"], None) for e in d if e["source"] == context_key and e["entity"] == ENTITY]
+        assert(len(contexts) == len(questions))
+        context_files.append(contexts)
+
+    question_file = "qa_rewritten"
+
+if(not "openai" in MODEL):
+    OPENAI_BATCH = False
+
+openai_client = None
+if("openai" in MODEL):
+    openai_client = openai.OpenAI(
+***REMOVED***
+    )
+
+gemini_client = None
+if("gemini" in MODEL):
+    google_api_key = os.environ["GEMINI_API_KEY"]
+    assert(len(google_api_key) > 0)
+    gemini_client = google_genai.Client(api_key=google_api_key)
+
+deepseek_client = None
+if("deepseek" in MODEL):
+    key = os.environ["DEEPSEEK_API_KEY"]
+    deepseek_client = openai.OpenAI(
+        api_key=key,
+        base_url="https://api.deepseek.com",
+    )
+
+claude_client = None
+if("claude" in MODEL):
+    claude_client = anthropic.Anthropic()
+
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+run_name = f"{'_'.join([URL_TO_NAME[k].lower().replace(' ', '_') for k in context_keys])}_{MODEL}_{RUN_NAME}"
+
+hf_names = {
+    "llama": "meta-llama/Llama-3.3-70B-Instruct",
+    "r1_llama": "deepseek-ai/DeepSeek-R1-Distill-Llama-70B",
+    "r1_llama_8B": "deepseek-ai/DeepSeek-R1-Distill-Llama-8B",
+    "llama_8B": "meta-llama/Llama-3.1-8B-Instruct",
+    "s1": "simplescaling/s1.1-32B",
+    "gemma": "google/gemma-3-27b-it",
+    "gemma_4B": "google/gemma-3-4b-it",
+}
+
+# Load the model and tokenizer
+if(MODEL in hf_names):
+    model_name = hf_names[MODEL]
+
+    quantization_config = BitsAndBytesConfig(
+        load_in_8bit=True,
+    )
+
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_name, 
+        padding_side="left",
+        pad_to_multiple_of=8, # Gemma likes this
+    )
+    
+    pipeline = transformers.pipeline(
+        "text-generation",
+        model=model_name,
+        #model_kwargs={"quantization_config": quantization_config, "torch_dtype": torch.float16},
+        model_kwargs={"torch_dtype": torch.bfloat16, "attn_implementation": "flash_attention_2"},
+        tokenizer=tokenizer,
+        device_map="auto",
+    )
+
+if(C4_JSONL is not None):
+    with open(C4_JSONL, "r") as fp:
+        jsons = fp.readlines()
+
+    c4_documents = [json.loads(j) for j in jsons]
+
+def parse_question(question):
+    q, a = question.split('\n')
+    q = q.strip()
+    a = a.strip()
+    return q, a
+
+def get_domain(url):
+        parsed_url = urlparse(url)
+        return parsed_url.netloc
+
+answers = []
+shuffled_ids = []
+correctness = []
+all_messages = []
+openai_batch = []
+for i, (_, question) in enumerate(questions):
+    if(i % 10 == 0):
+        print(i)
+
+    if(args.stop_after_k is not None and i == args.stop_after_k):
+        break
+
+    q, a = parse_question(question)
+   
+    context_tups = []
+    for context_file, context_key in zip(context_files, context_keys):
+        context = context_file[i]
+        context_tups.append((context, context_key))
+    
+    if(C4_JSONL is not None and NO_C4_DOCUMENTS > 0):
+        c4_sample = random.sample(c4_documents, NO_C4_DOCUMENTS)
+        context_tups.extend([((s["text"][:1000], None), s["metadata"]["url"]) for s in c4_sample])
+    
+    ct = list(enumerate(context_tups))
+    random.shuffle(ct)
+    context_tups = [t for _, t in ct]
+    shuffled_ids.append([idx for idx, _ in ct])
+
+    context_string = ""
+    for j, ((c, _), u) in enumerate(context_tups):
+        c = c.replace('\n', ' ')
+        domain = get_domain(u)
+        if(len(domain) == 0):
+            domain = "unknown"
+        context_string += f"Context document {j + 1} source: {domain}\nContext document {j + 1}: {c}\n\n"
+
+    messages = [
+        {"role": "system", "content": "You are an assistant that answers questions correctly. Write nothing but the answer to each question, or \"I don't know\" if you don't know the answer. Write exactly one answer per question (do not write \"or\"). You may (but do not have to) consult the provided context. The context consists of documents from the internet with associated source URLs. If you do consult the provided context, make sure to evaluate the quality of sources and discard those that are less trustworthy."},
+        {"role": "user", "content": f"{context_string}\nQuestion: {q}"}
+    ]
+
+    all_messages.append(messages)
+
+    if(WRITE_TO_DISK):
+        continue
+
+    if("llama" in MODEL):
+        outputs = pipeline(
+            messages,
+    #        temperature=0.,
+            temperature=None,
+            top_p=None,
+            do_sample=False,
+            max_new_tokens=256,
+        )
+        output_text = outputs[0]["generated_text"][-1]["content"]
+    elif("gemma" in MODEL):
+        while True:
+            try:
+                outputs = pipeline(
+                    messages,
+            #        temperature=0.,
+                    temperature=None,
+                    top_p=None,
+                    do_sample=False,
+                    max_new_tokens=256,
+                )
+                break
+            except RuntimeError:
+                print("Gemma error!")
+                continue
+        
+        output_text = outputs[0]["generated_text"][-1]["content"]
+    elif("gemini" in MODEL):
+        if(messages[0]["role"] == "system"):
+            messages[1]["content"] = messages[0]["content"] + '\n\n' + messages[1]["content"]
+            messages = messages[1:]
+
+        assert(len(messages) == 1)
+
+        while True:
+            try:
+                chat = gemini_client.chats.create(model=MODEL)
+                output_text = chat.send_message(messages[0]["content"]).text
+            except exceptions.ResourceExhausted as e:
+                print(f"Rate limit! Sleeping for {GOOGLE_SLEEP} seconds...")
+                time.sleep(GOOGLE_SLEEP)
+                continue
+            except Exception as e:
+                print(f"Unidentified Google exception: {e}")
+                exit()
+
+            break
+    elif("openai" in MODEL):
+        assert('_' in MODEL)
+        openai_model = MODEL.split('_')[-1]
+        for m in messages:
+                if(m["role"] == "system"):
+                    m["role"] = "developer"
+
+        if(not OPENAI_BATCH):
+            completion = openai_client.chat.completions.create(
+                model=openai_model,
+                messages=messages,
+            )
+            output_text = completion.choices[0].message.content
+        else:
+            batch_line = {
+                "custom_id": f"request-{i + 1}",
+                "method": "POST",
+                "url": "/v1/chat/completions",
+                "body": {
+                    "model": openai_model,
+                    "messages": messages,
+                }
+            }
+            openai_batch.append(batch_line)
+            continue
+    elif("deepseek" in MODEL):
+        completion = deepseek_client.chat.completions.create(
+            model=MODEL,
+            messages=messages,
+            stream=False,
+        )
+        #print(len(completion.choices[0].message.reasoning_content))
+        output_text = completion.choices[0].message.content
+    elif("claude" in MODEL):
+        system=None
+        if(messages[0]["role"] == "system"):
+            system = messages[0]["content"]
+            messages = messages[1:]
+
+        message = claude_client.messages.create(
+            model=MODEL,
+            max_tokens=128,
+            temperature=1,
+            system=system,
+#            thinking={
+#                "type": "enabled",
+#                "budget_tokens": 1024,
+#            },
+            messages=messages,
+        )
+
+        output_text = message.content[-1].text
+    elif("s1" in MODEL):
+        if(messages[0]["role"] == "system"):
+            messages[1]["content"] = messages[0]["content"] + '\n\n' + messages[1]["content"]
+            messages = messages[1:]
+        outputs = pipeline(
+            messages,
+    #        temperature=0.,
+            temperature=None,
+            top_p=None,
+            do_sample=False,
+            max_new_tokens=1024,
+        )
+        output_text = outputs[0]["generated_text"][-1]["content"]
+    else:
+        raise ValueError(f"\"{MODEL}\" is not a valid model.")
+
+    answers.append(output_text)
+
+if(WRITE_TO_DISK):
+    os.makedirs(PROMPT_DIR, exist_ok=True)
+    with open(os.path.join(PROMPT_DIR, f"{question_file}_{run_name}.pickle"), "wb") as fp:
+        pickle.dump(all_messages, fp, protocol=pickle.HIGHEST_PROTOCOL)
+
+    exit()
+
+if(OPENAI_BATCH):
+    os.makedirs(OPENAI_BATCH_DIR, exist_ok=True)
+    with open(os.path.join(OPENAI_BATCH_DIR, f"{question_file}_{run_name}.jsonl"), "w") as fp:
+        for b in openai_batch:
+            json.dump(b, fp)
+            fp.write('\n')
+    exit()
+
+with open(os.path.join(OUTPUT_DIR, f"{question_file}_{run_name}.pickle"), "wb") as fp:
+    pickle.dump(list(zip(answers, shuffled_ids)), fp, protocol=pickle.HIGHEST_PROTOCOL)
---
+++ smel/scripts/grade_answers.py
@@ -0,0 +1,179 @@
+import argparse
+import datasets
+from itertools import combinations
+import json
+import os
+import pickle
+import matplotlib.pyplot as plt
+import openai
+import torch
+import transformers
+from transformers import (
+    AutoTokenizer, 
+    AutoModelForCausalLM,
+    BitsAndBytesConfig,
+)
+from urllib.parse import urlparse
+
+from constants import (
+    DOMAINS,
+    URL_TO_NAME,
+    filter_combinations,
+)
+
+
+def parse_question(question):
+    q, a = question.split('\n')
+    q = q.strip()
+    a = a.strip()
+    return q, a
+
+
+parser = argparse.ArgumentParser()
+#parser.add_argument("--combo_id", type=int, default=0)
+parser.add_argument("--entity", type=str, default="agency")
+parser.add_argument("--model", type=str, default="llama")
+parser.add_argument("--use_local", action="store_true", default=False)
+args = parser.parse_args()
+
+ENTITY = args.entity
+MODEL = args.model
+
+RUN_NAME = f"{ENTITY}_ignoring"
+USE_CONTEXT = True
+FIG_DIR = f"results/{MODEL}_{RUN_NAME}"
+PICKLE_DIR = "pickles"
+OUTPUT_DIR = f"pickles/{MODEL}_{RUN_NAME}/"
+
+os.makedirs(FIG_DIR, exist_ok=True)
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+BATCH_SIZE = 64
+
+model_name = "meta-llama/Llama-3.3-70B-Instruct"
+
+def get_context_keys(no_docs, combo_id):
+    domain_combinations = list(combinations([url for _, url in DOMAINS], no_docs))
+     
+    if(no_docs == 2):
+        domain_combinations = [t for t in domain_combinations if filter_combinations(t)]
+    
+    print(list(enumerate(domain_combinations)))
+    context_keys = list(domain_combinations[combo_id])
+
+    return domain_combinations, context_keys
+
+if(args.use_local):
+    QUESTION_FILE = f"{ENTITY}_questions_filtered.pickle"
+    CONTEXT_FILES = [
+        f"{ENTITY}_questions_filtered_rewritten.pickle",
+        f"{ENTITY}_questions_filtered_{ENTITY}_questions_filtered_rewritten_corrupted.pickle",
+    ]
+    no_docs = len(CONTEXT_FILES)
+
+    with open(os.path.join(PICKLE_DIR, QUESTION_FILE), "rb") as fp:
+        questions = pickle.load(fp)
+   
+    question_file = os.path.basename(QUESTION_FILE).rsplit('.', 1)[0]
+else:
+    CONTEXT_FILES = ["qa_rewritten", "qa_corrupted"]
+    datasets = [datasets.load_dataset("gahdritz/smel", name=c, split="test") for c in CONTEXT_FILES]
+ 
+    no_docs = len(datasets)
+    
+    domain_combinations, context_keys = get_context_keys(no_docs, 0)
+ 
+    for d, context_key in zip(datasets, context_keys):
+        questions = [(None, '\n'.join([e["question"], e["answer"]])) for e in datasets[0] if e["source"] == context_key and e["entity"] == ENTITY]
+
+    question_file = CONTEXT_FILES[0]
+
+# Load the model and tokenizer
+quantization_config = BitsAndBytesConfig(
+    load_in_8bit=True,
+)
+
+tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side="left")
+
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model_name,
+    #model_kwargs={"quantization_config": quantization_config, "torch_dtype": torch.float16},
+    model_kwargs={"torch_dtype": torch.bfloat16, "attn_implementation": "flash_attention_2"},
+    tokenizer=tokenizer,
+    device_map="auto",
+)
+
+for combo_id in range(len(domain_combinations)):
+    print(f"Combo ID: {combo_id}")
+
+    context_keys = list(domain_combinations[combo_id])
+    run_name = f"{'_'.join([URL_TO_NAME[k].lower().replace(' ', '_') for k in context_keys])}_{MODEL}_{RUN_NAME}"
+    
+    answer_file_name = f"{question_file}_{run_name}.pickle"
+    
+    with open(os.path.join(OUTPUT_DIR, answer_file_name), "rb") as fp:
+        answers = pickle.load(fp)
+    
+    all_messages = []
+    for i, ((_, question), (model_answer, _)) in enumerate(zip(questions, answers)):
+        if(i % 10 == 0):
+            print(i)
+    
+        q, a = parse_question(question)
+       
+        messages = [
+            {"role": "system", "content": "You are an assistant that grades tests. Given a question, an answer key, and the student's answer, write \"Correct\" if the answer is correct and \"Incorrect\" otherwise. Do not write anything else. Do not give partial credit, and do not accept \"shotgun\" answers that contain more than one guess unless one answer is clearly identified as the final one."},
+            {"role": "user", "content": f"Question: {q}\nAnswer key: {a}\nStudent answer: {model_answer}"}
+        ]
+    
+        all_messages.append(messages)
+     
+    outputs = pipeline(
+        all_messages,
+        temperature=None,
+        max_new_tokens=256,
+        batch_size=BATCH_SIZE,
+    )
+    output_texts = [o[0]["generated_text"] for o in outputs]
+    
+    correctness = [o[-1]["content"] for o in output_texts]
+    
+    answer_file = os.path.basename(answer_file_name).rsplit('.', 1)[0]
+    
+    with open(os.path.join(PICKLE_DIR, f"{question_file}_{answer_file}_graded.pickle"), "wb") as fp:
+        pickle.dump(correctness, fp, protocol=pickle.HIGHEST_PROTOCOL)
+    
+    os.makedirs(FIG_DIR, exist_ok=True)
+    fraction_correct = sum([c == "Correct" for c in correctness]) / len(correctness)
+    fraction_incorrect = 1 - fraction_correct
+    fraction_or = sum(["or" in c for c in answers]) / len(correctness)
+    fraction_abstention = sum(["I don't know" in c for c in answers]) / len(answers)
+    
+    fig, ax = plt.subplots(figsize=(10, 6))
+    bars = ax.bar(["Correct", "Incorrect", "or", "Abstentions"], [fraction_correct, fraction_incorrect, fraction_or, fraction_abstention])
+    
+    run_name = answer_file_name.rsplit('.', 1)[0]
+    title = run_name
+    ax.set_title(title)
+    
+    for bar in bars:
+        height = round(bar.get_height(), 3)
+        ax.text(bar.get_x() + bar.get_width()/2., height,
+            f'{height:}',
+            ha='center', 
+            va='bottom'
+        )
+    
+    ax.grid(axis='y', linestyle='--', alpha=0.7)
+    
+    cxt_name = '_'.join([s.rsplit('.', 1)[0] for s in [question_file, *CONTEXT_FILES]])
+    fig_dir = os.path.join(FIG_DIR, cxt_name)
+    os.makedirs(fig_dir, exist_ok=True)
+    fig_name = f"{run_name}.png"
+    fig_path = os.path.join(fig_dir, fig_name)
+    plt.savefig(fig_path, bbox_inches="tight")
+   
+    print(f"Correct: {fraction_correct}")
+    print(f"Incorrect: {fraction_incorrect}")
+    print(f"Abstentions: {fraction_abstention}")
